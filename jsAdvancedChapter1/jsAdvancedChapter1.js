6e70406257f8a7c9534778d3cf07e27de4e191d9
Eriniss <jhs2195@gmail.com>
2022-06-04 21:47:51 +0900

    1.14

--- jsAdvancedChapter1/jsAdvancedChapter1.js
+++ jsAdvancedChapter1/jsAdvancedChapter1.js
@@ -82,6 +82,152 @@
   }
   f1();
 }
+// 자바스크립트가 실행될 때 전역 실행 컨텍스트가 만들어 진다
+// 함수가 실행될 때 함수 실행 컨텍스트가 만들어 진다.
 
 
+// * this가 가리키는 것
+
+// 함수가 호출되는 상황은 4가지가 있다.
+// 1) 일반 함수 호출, 2) 메서드 호출, 3) 생성자 호출, 4) 간접 호출(call, apply, bind 등)
+
+// 그외 콜백 함수의 호출이 있다.
+// 콜백 함수는 특정 동작 이후 불려지는 함수이다.
+// 보통 다른 함수의 인자로 보내지는 함수를 의미한다.
+// setTimeOut 메서드도 콜백함수를 이용한다.
+
+{
+  function myFunc() {
+    console.log('myFunc called')
+  }
+
+  myFunc() // 함수를 직접 호출.
+
+  const o = {
+    name: 'Daniel',
+    printName: function() {
+      console.log(this.name);
+    }
+  }
+
+  o.printName() // 객체의 메서드를 호출
+}
+
+{
+  function Person(name) {
+    this.name = name;
+    this.printName = function() {
+      console.log(this.name);
+    }
+  }
+
+  const p = new Person('Daniel'); // 생성자 호출
+  setTimeout(p.printName.bind(p, 1000)); // 간접 호출
+}
+//함수는 이렇듯 다양한 상황(환경)에서 호출될 수 있다.
+// 함수의 호출 환경에 따라 this는 동적으로 세팅된다.
+// 이렇게 this가 환경에 따라 바뀌는 것을 도적 바인딩(dynamic binding)이라 한다.
+// bind, apply, call 등으로 this가 가리키는 것을 조작할 수 있다.
+
+
+// * 화살표 함수와 일반 함수의 this
+
+// 화살표 함수의 this는 호출된 함수를 둘러싼 실행 컨텍스트를 가리킨다.
+// 일반 함수의 this는 새롭게 생성된 실행 컨텍스트를 가리킨다.
+
+{
+  const o = {
+    method() {
+      console.log("context : ", this) // o 일반함수의 this는 항상 global을 가리킨다.
+      let f1 = function () {
+        console.log("[f1] this :", this);
+      }
+      let f2 = () =>
+      console.log("[f2] this : ", this)
+      f1() // global, 일반 함수의 this는 항상 global을 가리킨다.
+      f2() // o, 화살표 함수의 this는 항상 한단계 위의 객체를 가리킨다.
+    }
+  }
+  o.method();
+}
+// 화살표 함수의 this는 정해지면 바꿀 수 없다.
+// call, bind, apply를 사용해도 바뀌지 않는다.
+// setTimeout 등 this가 바뀌는 상황에서 유용하다.
+{
+  window.name = 'Daniel';
+  let o = { name : 'Kim' };
+
+  let arrowFunction = (perfix) => console.log(prefix + this.name);
+
+  arrowFunction('Dr. ') // Dr. Daniel
+  arrowFunction.bind(o)('Dr. '); // Dr. Daniel
+  arrowFunction.apply(o)('Dr. '); // Dr. Daniel
+  arrowFunction.call(o)('Dr. '); // Dr. Daniel
+}
+// 위의 예제처럼 어떤 간접 호출을 사용해도 this값을 바꿀 수 없다.
+
+
+// * 자바스크립트 Closure
+
+// 일급 객체란, 다른 변수처럼 대상을 다룰 수 있는 것을 말한다.
+// 자바스크립트에서 함수는 일급 객체 이다.
+// 즉, 자바스크립트에서 함수는 변수처럼 다룰 수 있다.
+
+// 자바스크립트 클로저는, 함수의 일급 객체 성질을 이용한다.
+// 함수가 생성될 때, 함수 내부에서 사용되는 변수들이 외부에 존재하는 경우 그 변수들은 함수의 스코프에 저장된다.
+// 함수와 함수가 사용하는 변수들을 저장한 공간을 클로저(closure)라 한다.
+
+{
+  let rate = 1.05;
+
+  function app() {
+    let base = 10;
+
+    return function (price) {
+      return price * rate + base;
+    }
+  }
+
+  const getPrice = app();
+  getPrice(120); // 136
+}
+// base는 app 함수 내부, rate는 app 함수 내부의 스코프에 존재한다.
+// 함수가 참조하는 변수는 실행 시점에 실행 컨텍스트에 의해 스코프가 결정된다.
+
+
+// * Rest, Spread Operator
+
+// Rest
+// 함수의 인자, 배열, 객체 중 나머지 값을 묶어 사용하도록 한다.
+
+{
+  function findMin(...rest) {
+    return rest.reduce((a, b) => a < b ? a : b);
+  }
+
+  findMin(7, 3, 5, 2, 4, 1) // 1
+}
+// 함수 인자 rest operator는 인자들을 배열로 묶는다.
+// restdpsms tntwkemfdl qoduffh ekarlsek.
+// reduce 함수로 min 값을 리턴한다.
+
+
+// Spread Operator
+// 묶인 배열 혹은 객체를 각각의 필드로 변환한다.
+
+{
+  let o = {
+    name: "Daniel",
+    age: 23,
+    address: "Street",
+    job: "Software Enginner",
+  };
+
+  let o2 = { ...o, name: "Tom", age: 24 }; // ...가 스프레드 오퍼레이터다. ...이 앞에 위치하게 되면 ... 뒤의 데이터가 앞의 o 객체의 데이터를 덮어 씌우게 된다.
+
+  let o3 = { name: "Tom", age: 24, ...o }; // ...이 뒤에 위치하게 되면 스프레드된 o 객체가 o3를 덮어 씌우게 된다.
+
+  o2.age; // 24
+  o3.name; // Daniel
+}
 
--- test.js
+++ test.js
@@ -1,15 +1,15 @@
-// 자바스크립트 코드의 실행 3
+{
+  let o = {
+    name: "Daniel",
+    age: 23,
+    address: "Street",
+    job: "Software Enginner",
+  };
 
-let o = {
-  name: 'Daniel',
-  method: function(number) {
-    return this.name.repeat(number);
-  }
-};
+  let o2 = { ...o, name: "Tom", age: 24 }; // ...가 스프레드 오퍼레이터다. 스프레드 뒤의 데이터가 앞의 o 객체의 데이터를 덮어 씌우게 된다.
 
-function myFunc() {
-  let n = 10;
-  return console.log(o.method(n)); 
+  let o3 = { name: "Tom", age: 24, ...o }; // ...이 뒤에 위치하게 되면 스프레드된 o 객체가 o3를 덮어 씌우게 된다.
+
+  console.log(o2.age); // 24
+  console.log(o3.name); // Daniel
 }
-
-myFunc() // 'Daniel'이 10번 출력된다.
